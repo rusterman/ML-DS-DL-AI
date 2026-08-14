a3f912c4258b3b81ef8d65483b42f92d5afd7685
Eldeniz Mirzeyev <eldenizmirzeyev712@gmail.com>
2026-08-14 16:10:25 +0400

    Add double summation multiplication exercise

--- Mathematics/Equation-Types/sigma-notation-exercises.py
+++ Mathematics/Equation-Types/sigma-notation-exercises.py
@@ -7,6 +7,16 @@
     Compute the sum of integers from 1 to 50. Write code to compute and print the result.
 """
 # Your Solution...
+print("1.Simple Summation: ∑₍ᵢ₌₁₎ⁿ i")
+def simple_summation(n):
+    result = 0
+    for i in range (1, n + 1):
+        result += i
+    return result
+
+n = 50 
+print(f"Sum of i from 1 to {n} is {simple_summation(n)}")
+print()
 
 
 """
@@ -17,7 +27,12 @@
     Compute the sum of even numbers from 2 to 20 using sum() and a generator expression. Print the result.
 """
 # Your Solution...
+print("2. Summation with Generator Expressio")
+def summation_range(a, b, func= lambda x: x):
+    return sum(func(i) for i in range (a, b + 1, 2))
 
+print(f"Sum of squares from 2 to 20 is {summation_range(2, 20)}")
+print()
 
 """
     3. Symbolic Summation with Sympy
@@ -27,8 +42,13 @@
     Symbolically compute ∑₍ᵢ₌₁₎ⁿ i² using sympy, simplify the expression, and print both the symbolic formula and its value for n=10.
 """
 # Your Solution...
-
-
+from sympy import symbols, summation
+print("3. Symbolic Summation with Sympy")
+i, n = symbols("i n")
+expr = summation (i ** 2, (i , 1, n))
+print(f"Dustur: {expr}")
+print(f"1 to 10: {expr.subs(n, 10)}")
+print()
 """
     4. Double Summation for Multiplication
        Express a * b as ∑₍ᵢ₌₁₎ᵃ ∑₍ⱼ₌₁₎ᵇ 1.
@@ -38,3 +58,13 @@
     For example, for a=4 and b=5, your code should compute 20 by summing 1 over the appropriate ranges. Print the result.
 """
 # Your Solution...
+print("4. Double Summation for Multiplication")
+def Sum(a, b):
+    result = 0
+    for i in range (1, a + 1):
+        for j in range (1, b + 1):
+            result += 1
+    return result
+print(f"a=4 b=5 result: {Sum(4, 5)}")
+    
+
